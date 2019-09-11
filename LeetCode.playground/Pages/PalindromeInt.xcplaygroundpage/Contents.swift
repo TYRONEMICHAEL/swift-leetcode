79d40895184fe18289c009c8feafb959a50a8b66
Tyrone Avnit <tyronemichael@gmail.com>
2019-09-11 17:00:12 +0200

    Add better solution for determining if an Int is a paldindrome

--- LeetCode.playground/Pages/PalindromeInt.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/PalindromeInt.xcplaygroundpage/Contents.swift
@@ -2,22 +2,23 @@
 // An integer is a palindrome when it reads the same backward as forward.
 
 func isPalindrome(_ x: Int) -> Bool {
-    guard x >= 0 else {
+    if x < 0 || (x % 10 == 0 && x != 0) {
         return false
     }
 
     var num = x
     var reversedNum = 0
 
-    while num > 0 {
+    while num > reversedNum {
         let digit = num % 10
         reversedNum = (reversedNum * 10) + digit
         num = num / 10
     }
 
-    return reversedNum == x
+    return reversedNum == num || num == reversedNum / 10
 }
 
 assert(isPalindrome(0))
+assert(isPalindrome(12321))
 assert(isPalindrome(10) == false)
-assert(isPalindrome(121))
+
